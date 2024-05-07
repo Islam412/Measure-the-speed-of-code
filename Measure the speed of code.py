53ff8ecb5945b623e8f944ac7a44b82fc6092235
Islam Hamdy <islam.124.hamdy@gmail.com>
2024-05-07 03:56:46 +0300

    add decrator time Measure off sums

--- Measure the speed of code.py
+++ Measure the speed of code.py
@@ -1,14 +1,46 @@
+# import time
+
+
+# def caculater_time(func):
+#     def inner(a,b):
+        
+#         print('start ....')
+#         result=func(a,b)
+#         print('end ...')
+#         return result
+        
+#     return inner
+
+
+# @caculater_time
+# def mysum(x,y):
+#     print(x+y)
+    
+# mysum(3,4)
+
+
+
 import time
 
 
 def caculater_time(func):
+    def inner(a,b):
+        
+        start = time.time()
+        
+        result=func(a,b)
+        
+        end = time.time()
+        
+        print(f'total time : {end-start}')
+        return result
+        
+    return inner
+
+
+@caculater_time
+def mysum(x,y):
+    time.sleep(5)
+    return x+y
     
-    def inner(*args,**kwargs):
-        
-        print('start ....')
-        func(*args,**kwargs)
-        print('end ...')
-        
-    return inner()
-
-
+mysum(3,4)
